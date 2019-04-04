b516bf932cad985315cda2434ba448dcad9cf45e
Rokhuda <47817890+Rokhuda@users.noreply.github.com>
2019-04-04 14:03:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,103 +1,87 @@
 //1
-function dayTime(){
-function dayy(){
-  let day = new Date();
-  let today = day.getDate();
-  let weekDays = ["Sunday" , "Monday" , "Tuesday", "Wednesday", "Thursday" , "Friday" , "Saturday"];     
-  return weekDays[today];
-}
-
-function timee(){
- let day = new Date();    
- let hours = day.getHours();
- let minutes =day.getMinutes();
- let seconds = day.getSeconds();
- let meridies;
- 
-
-if (hours<12)
-{
-
-   meridies = "AM"
-}
-else if (hours > 12)
-{
-   hours = hours - 12;
-   meridies ="PM";
-}
-else {
-  
- meridies = "PM";
-}
-return hours +" "+ meridies+" : "+minutes+" : "+" "+seconds;
+function dayTime() {
+  function dayy() {
+    let day = new Date();
+    let today = day.getDate();
+    let weekDays = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    return weekDays[today];
   }
 
+  function timee() {
+    let day = new Date();
+    let hours = day.getHours();
+    let minutes = day.getMinutes();
+    let seconds = day.getSeconds();
+    let meridies;
 
-console.log( "Today is: " + dayy()+" . Current time is : " +timee());
+    if (hours < 12) {
+      meridies = "AM";
+    } else if (hours > 12) {
+      hours = hours - 12;
+      meridies = "PM";
+    } else {
+      meridies = "PM";
+    }
+    return hours + " " + meridies + " : " + minutes + " : " + " " + seconds;
+  }
+  let dateTime = "Today is: " + dayy() + " . Current time is : " + timee();
 
+  document.getElementById("currentDate").innerHTML = dateTime;
 }
 
 // 2
-function loadURL(){
-  let getUrl = document.URL;
-  return getUrl;
+function loadURL() {
+  let getURL = document.URL;
+  document.getElementById("currentURL").innerHTML = getURL;
 }
 //3
 
-
-function checkWord(){
-    let sentence = prompt("enter a sentence");
-    let word = sentence.startsWith("Java");
-    return word;
+function checkWord() {
+  let sentence = document.getElementById("javaInput").value;
+  let word = sentence.startsWith("Java");
+  document.getElementById("javaCheck").innerHTML = word;
 }
-console.log(checkWord());
-
 
 //4
 
-
 function largest() {
-    let num1 = (prompt('Enter a number'));
-    let num2 = (prompt('Enter a number'));
-    let num3 = (prompt('Enter a number'));
-    let large = Math.max(num1, num2, num3);
-    return large
-  }
-    console.log(largest());
-
+  let num1 = document.getElementById("1stBox").value;
+  let num2 = document.getElementById("2ndBox").value;
+  let num3 = document.getElementById("3rdBox").value;
+  let largeNum = Math.max(num1, num2, num3);
+  document.getElementById("large").innerHTML = largeNum;
+}
 
 //5
 
-
-
-
 function capitalText() {
-  
-  text = prompt("Tell me your deepest darkest secret");
-  sentence = text.split(' ');
-   let i = 0;
- 
-  for ( i = 0; i < sentence.length; i++) {
-    sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].slice(1); 
-  }
-  return sentence.join(' ');
-}
-titleCasestr();
+  text = document.getElementById("capiText").value;
+  sentence = text.split(" ");
+  let i = 0;
 
+  for (i = 0; i < sentence.length; i++) {
+    sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].slice(1);
+  }
+  joinedCapi = sentence.join(" ");
+  document.getElementById("capi").innerHTML = joinedCapi;
+}
 
 //6
 
-
-let num = prompt( "Enter a number");
-
-function time_convert()
- {
+function time_convert() {
+  let num = document.getElementById("convertTime").value;
   let hours = Math.floor(num / 60);
   let minutes = num % 60;
-  return hours + ":" + minutes;
+  convertedTime = hours + ":" + minutes;
+  document.getElementById("convert").innerHTML = convertedTime;
 }
-
-console.log(time_convert());
-
 
 
